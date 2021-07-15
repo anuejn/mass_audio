82f5ce23d62a02abb1009c80df1dc396b5b2c964
Philipp Mandler <info@philipp-mandler.com>
2021-07-15 21:19:14 +0200

    improve env setup script

--- setup_env.sh
+++ setup_env.sh
@@ -2,4 +2,12 @@
 
 export ADF_PATH="$(pwd)/esp-adf/"
 export IDF_PATH="$(pwd)/esp-adf/esp-idf"
+
+virtualenv --python 3 --system-site-packages .venv
+source .venv/bin/activate
+
+# prepare esp-idf
+"${IDF_PATH}/tools/idf_tools.py" --non-interactive install
+"${IDF_PATH}/tools/idf_tools.py" --non-interactive install-python-env
+
 . ./esp-adf/esp-idf/export.sh
